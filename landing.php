e95769eca8feee539e263e7d274362f3bf081aea
hughstimson <hugh@hughstimson.org>
2017-11-27 18:05:26 -0800

    [WIP] Starting a throwback themed landing page.
    Copy/pasting from an HTML-only version of hughstimson.org from 2005 (?). Doesn't work yet.

---
+++ landing.php
@@ -0,0 +1,136 @@
+<?php ?>
+/**
+* Created by PhpStorm.
+* User: hstimson
+* Date: 2017-11-27
+* Time: 1:39 PM
+*/
+
+<html>
+<head>
+    <title>hughstimson.org</title>
+</head>
+<body>
+<center>
+    <br>
+    <p><b>hughstimson.org</b></p>
+    <img src="/web/20041206015037im_/http://hughstimson.org/star.70.png">
+    <p>
+        <a href="https://web.archive.org/web/20041206015037/http://hughstimson.org/blog">weblog</a> |
+        <a href="https://web.archive.org/web/20041206015037/http://www.hughstimson.org/gallery/">photo gallery</a> |
+        <a href="https://web.archive.org/web/20041206015037/http://www.hughstimson.org/resume">resume</a>
+    </p>
+    <p>
+        <a href="https://web.archive.org/web/20041206015037/http://www.reservaloscedros.org/">Reserva Los Cedros
+            website</a> |
+        <a href="https://web.archive.org/web/20041206015037/http://www.sea-to-sky.net/">Sea to Sky Freenet</a>
+
+    </p>
+    <p>Contact:<br>
+        Hugh Stimson<br>
+        1500 Remount Rd,
+        Front Royal VA 22630,
+        USA<br>
+        hugh * hughstimson * org<br>
+        1 (540) 635 6533</p>
+
+    <font size="-1">
+        <table width="85%" cellpadding="17">
+            <tr>
+                <td width="50%">
+                    <font size="-1">
+                        <p>Civilizations in decline are consistently characterized by a tendency
+                            toward standardization and uniformity. Conversely, during the growth
+                            stage of civilization, the tendency is toward differentiation and
+                            diversity.<br>
+                            -- <i>Arnold Toynbee, <u>A Study of History</u></i></p>
+                </td>
+                <td width="50%"><font size="-1">
+                        <p>We have to learn what we can, but remain mindful that our knowledge not close the circle,
+                            closing out the void, so that we forget that what we do not know remains boundless, without
+                            limit or bottom, and that what we know may have to share the quality of being known with
+                            what denies it. What is seen with one eye has no depth.<br>
+                            -- <i>Stone Telling, <u>Always Coming Home</u>, Ursula K. Leguin</i></p>
+                </td>
+            </tr>
+            <tr>
+                <td width="50%"><font size="-1">
+                        <p>As the piston continues to rise, creating vacuum in the crankcase, the
+                            inlet port is uncovered and thus the cycle is completed and repeated.<br>
+                            -- <i>from "The Two Stroke Engine", <u>Haynes Motorcyle Basics Techbook</u></i></p>
+                </td>
+                <td><font size="-1">
+                        <p>If we knew what it was were doing, it wouldn't be called research.<br>
+                            <i>-- Albert Einstein</i></p>
+                </td>
+            </tr>
+            <tr>
+                <td><font size="-1">
+                        <p>Very frankly, I am opposed to people being programmed by others. My whole approach in
+                            broadcasting has always been 'You are an important person just the way you are. You can make
+                            healthy decisions.' Maybe I'm going on too long, but I just feel that anything that allows a
+                            person to be more active in the control of his or her life, in a healthy way, is
+                            important.<br>
+                            -- <i>From Fred (Mr.) Rogers testimony to the
+                                Supreme Court in defense of fair use copying in the Sony Betamax case</i></p>
+                </td>
+                <td><font size="-1">
+                        <p>What kind of world is it<br>
+                            Comes headlong at you and swerves at the last possible second?<br>
+                            It's this one.<br>
+                            Well it's this one.<br>
+                            -- <i>The Mountain Goats, <u>Song for Dana Plato</u></i></p>
+                </td>
+            </tr>
+            <tr>
+                <td><font size="-1">
+                        <p>I certainly had no feeling for harmony, and Schoenberg thought that
+                            that would make it impossible for me to write music. He said, 'You'll
+                            come to a wall you won't be able to get through.' So I said, 'I'll
+                            beat my head against that wall.'<br>
+                            -- <i>John Cage</i></p>
+                </td>
+
+                <td><font size="-1">
+                        <p>It is an ultimate act of rudeness to find fault with anything which is given to us in a
+                            spirit of love. So a modern, secular education is often painful. By it's very nature, it
+                            invites us to question the wisdom of the ones we love. Too bad.<br>
+                            -- <i>Kurt Vonnegut, address to the
+                                graduating class of Hobart and William Smith Colleges, Geneva, New York 1974, from <u>Palm
+                                    Sunday</u></i></p>
+                </td>
+            </tr>
+            <tr>
+                <td><font size="-1">
+                        <p>Life is not ever either or. It's and and and and and."<br>
+                            -- <i>Philip Roth</i></p>
+                </td>
+                <td><font size="-1">
+                        <p>He almost waved away the tangible beauty of the hour like a crowd of persistent friends,
+                            almost said to the lingering afterglow: 'Thank you, thank you, I'll be all right.'<br>
+                            -- <i>Boris Pasternak, <u>Doctor Zhivago</u></i></p>
+                </td>
+            </tr>
+            <tr>
+                <td width="50%"><font size="-1">
+                        <p>'One whom the dragons will speak with,' he said, 'that is a
+                            dragonlord, or at least that is the center of the matter. It's not a
+                            trick of mastering the dragons, as most people think. Dragons have no
+                            masters.'<br>
+                            -- <i>Ged, <u>The Tombs of Atuan</u>, Ursula K Le Guin</i></p>
+
+                <td><font size="-1">
+                        <p>Was it me said you were crazy?<br>
+                            -- <i>Kate Bush, <u>Rocket's Tail</u></i></p>
+        </table>
+</center>
+</font>
+</body>
+</html>
+
+
+
+
+
+
+
