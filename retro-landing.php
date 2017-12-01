ed67d9b7181f0fd0c58cab5c8bba2c341a458182
hughstimson <hugh@hughstimson.org>
2017-11-30 21:16:56 -0800

    Refactoring to grid-based instead of table-based layout.

--- retro-landing.php
+++ retro-landing.php
@@ -2,110 +2,172 @@
 <html>
 <head>
     <title>hughstimson.org</title>
-    <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>" type="text/css" media="screen" />
+    <link rel="stylesheet"
+          href="<?php bloginfo('template_url'); ?>/lib/bootstrap-4.0.0-beta.2-dist/css/bootstrap-grid.min.css"
+          type="text/css" media="screen">
+    <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>" type="text/css" media="screen"/>
 </head>
 <body>
-<div id="retro-container">
-    <br>
-    <p><b>hughstimson.org</b></p>
-    <img src="<?php bloginfo('template_url'); ?>/images/star.70.png" />
-    <p>
-        <a href="<?php bloginfo('url'); ?>/blog">weblog</a> |
-        <a href="//twitter.com/hughstimson">twitter account</a> |
-        <a href="//essa.com">job</a> |
-        <a href="//geocology.ca">old job</a>
-    </p>
-    <font size="-1">
-        <table width="85%" cellpadding="17">
-            <tr>
-                <td width="50%">
-                    <font size="-1">
-                        <p>Civilizations in decline are consistently characterized by a tendency
-                            toward standardization and uniformity. Conversely, during the growth
-                            stage of civilization, the tendency is toward differentiation and
-                            diversity.<br>
-                            -- <i>Arnold Toynbee, <u>A Study of History</u></i></p>
-                </td>
-                <td width="50%"><font size="-1">
-                        <p>We have to learn what we can, but remain mindful that our knowledge not close the circle,
-                            closing out the void, so that we forget that what we do not know remains boundless, without
-                            limit or bottom, and that what we know may have to share the quality of being known with
-                            what denies it. What is seen with one eye has no depth.<br>
-                            -- <i>Stone Telling, <u>Always Coming Home</u>, Ursula K. Leguin</i></p>
-                </td>
-            </tr>
-            <tr>
-                <td width="50%"><font size="-1">
-                        <p>As the piston continues to rise, creating vacuum in the crankcase, the
-                            inlet port is uncovered and thus the cycle is completed and repeated.<br>
-                            -- <i>from "The Two Stroke Engine", <u>Haynes Motorcyle Basics Techbook</u></i></p>
-                </td>
-                <td><font size="-1">
-                        <p>If we knew what it was were doing, it wouldn't be called research.<br>
-                            <i>-- Albert Einstein</i></p>
-                </td>
-            </tr>
-            <tr>
-                <td><font size="-1">
-                        <p>Very frankly, I am opposed to people being programmed by others. My whole approach in
-                            broadcasting has always been 'You are an important person just the way you are. You can make
-                            healthy decisions.' Maybe I'm going on too long, but I just feel that anything that allows a
-                            person to be more active in the control of his or her life, in a healthy way, is
-                            important.<br>
-                            -- <i>From Fred (Mr.) Rogers testimony to the
-                                Supreme Court in defense of fair use copying in the Sony Betamax case</i></p>
-                </td>
-                <td><font size="-1">
-                        <p>What kind of world is it<br>
-                            Comes headlong at you and swerves at the last possible second?<br>
-                            It's this one.<br>
-                            Well it's this one.<br>
-                            -- <i>The Mountain Goats, <u>Song for Dana Plato</u></i></p>
-                </td>
-            </tr>
-            <tr>
-                <td><font size="-1">
-                        <p>I certainly had no feeling for harmony, and Schoenberg thought that
-                            that would make it impossible for me to write music. He said, 'You'll
-                            come to a wall you won't be able to get through.' So I said, 'I'll
-                            beat my head against that wall.'<br>
-                            -- <i>John Cage</i></p>
-                </td>
+<div class="container" id="retro-container">
+    <div class="row">
+        <div class="col" id="retro-masthead-div">
+            <h1 id="retro-title-h1">hughstimson.org</h1>
+            <img src="<?php bloginfo('template_url'); ?>/images/star.70.png"/>
+            <p>
+                <a href="<?php bloginfo('url'); ?>/blog">weblog</a> |
+                <a href="//twitter.com/hughstimson">twitter account</a>
+            </p>
+            <p>
+                <a href="//ca.linkedin.com/in/hughstimson">resume</a> |
+                <a href="//essa.com">job</a> |
+                <a href="//geocology.ca">old job</a>
+            </p>
+        </div>
+    </div>
+    <div class="row">
+        <div class="retro-quote-div col-12 col-md-6">
+            <div class="retro-content-div">Civilizations in decline are consistently characterized by a tendency
+                toward standardization and uniformity. Conversely, during the growth
+                stage of civilization, the tendency is toward differentiation and
+                diversity.
+            </div>
+            <div class="retro-citation-div">
+                &mdash; Arnold Toynbee, <span class="retro-title-span">A Study of History</span>
+            </div>
+        </div>
 
-                <td><font size="-1">
-                        <p>It is an ultimate act of rudeness to find fault with anything which is given to us in a
-                            spirit of love. So a modern, secular education is often painful. By it's very nature, it
-                            invites us to question the wisdom of the ones we love. Too bad.<br>
-                            -- <i>Kurt Vonnegut, address to the
-                                graduating class of Hobart and William Smith Colleges, Geneva, New York 1974, from <u>Palm
-                                    Sunday</u></i></p>
-                </td>
-            </tr>
-            <tr>
-                <td><font size="-1">
-                        <p>Life is not ever either or. It's and and and and and."<br>
-                            -- <i>Philip Roth</i></p>
-                </td>
-                <td><font size="-1">
-                        <p>He almost waved away the tangible beauty of the hour like a crowd of persistent friends,
-                            almost said to the lingering afterglow: 'Thank you, thank you, I'll be all right.'<br>
-                            -- <i>Boris Pasternak, <u>Doctor Zhivago</u></i></p>
-                </td>
-            </tr>
-            <tr>
-                <td width="50%"><font size="-1">
-                        <p>'One whom the dragons will speak with,' he said, 'that is a
-                            dragonlord, or at least that is the center of the matter. It's not a
-                            trick of mastering the dragons, as most people think. Dragons have no
-                            masters.'<br>
-                            -- <i>Ged, <u>The Tombs of Atuan</u>, Ursula K Le Guin</i></p>
+        <div class="retro-quote-div col-12 col-md-6">
+            <div class="retro-content-div">We have to learn what we can, but remain mindful that our knowledge not
+                close the
+                circle,
+                closing out the void, so that we forget that what we do not know remains boundless, without
+                limit or bottom, and that what we know may have to share the quality of being known with
+                what denies it. What is seen with one eye has no depth.
+            </div>
+            <div class="retro-citation-div">&mdash; Stone Telling, <span
+                        class="retro-title-span">Always Coming Home</span>, Ursula K. Leguin
+            </div>
+        </div>
 
-                <td><font size="-1">
-                        <p>Was it me said you were crazy?<br>
-                            -- <i>Kate Bush, <u>Rocket's Tail</u></i></p>
-        </table>
+        <div class="retro-quote-div col-12 col-md-6">
+            <div class="retro-content-div">As the piston continues to rise, creating vacuum in the crankcase,
+                the
+                inlet port is uncovered and thus the cycle is completed and repeated.
+            </div>
+            <div class="retro-citation-div">&mdash; from "The Two Stroke Engine", <span class="retro-title-span">Haynes Motorcyle Basics Techbook</span>
+            </div>
+        </div>
+
+        <div class="retro-quote-div col-12 col-md-6">
+            <div class="retro-content-div">If we knew what it was were doing, it wouldn't be called
+                research.
+            </div>
+            <div class="retro-citation-div">&mdash; Albert Einstein</div>
+        </div>
+
+        <div class="retro-quote-div col-12 col-md-6">
+            <div class="retro-content-div">Very frankly, I am opposed to people being programmed by others.
+                My whole approach in
+                broadcasting has always been 'You are an important person just the way you are. You can make
+                healthy decisions.' Maybe I'm going on too long, but I just feel that anything that allows a
+                person to be more active in the control of his or her life, in a healthy way, is
+                important.
+            </div>
+            <div class="retro-citation-div">&mdash; From Fred (Mr.) Rogers testimony to the
+                Supreme Court in defense of fair use copying in the Sony Betamax case
+            </div>
+        </div>
+
+        <div class="retro-quote-div col-12 col-md-6">
+            <div class="retro-content-div">
+                What kind of world is it<br>
+                Comes headlong at you and swerves at the last possible second?<br>
+                It's this one.<br>
+                Well it's this one.<br>
+            </div>
+            <div class="retro-citation-div">&mdash; The Mountain Goats, <span class="retro-title-span">Song for Dana Plato</span>
+            </div>
+        </div>
+
+        <div class="retro-quote-div col-12 col-md-6">
+            <div class="retro-content-div">I certainly had no feeling for harmony, and Schoenberg
+                thought that
+                that would make it impossible for me to write music. He said, 'You'll
+                come to a wall you won't be able to get through.' So I said, 'I'll
+                beat my head against that wall.'
+            </div>
+            <div class="retro-citation-div">&mdash; John Cage</div>
+        </div>
+
+        <div class="retro-quote-div col-12 col-md-6">
+            <div class="retro-content-div">It is an ultimate act of rudeness to find fault with
+                anything which is given to us in a
+                spirit of love. So a modern, secular education is often painful. By it's very
+                nature, it
+                invites us to question the wisdom of the ones we love. Too bad.
+            </div>
+            <div class="retro-citation-div">&mdash; Kurt Vonnegut, address to the
+                graduating class of Hobart and William Smith Colleges, Geneva, New York
+                1974, from <span class="retro-title-span">Palm
+                    Sunday</span></div>
+        </div>
+
+        <div class="retro-quote-div col-12 col-md-6">
+            <div class="retro-content-div">Life is not ever either or. It's and and and and
+                and."
+            </div>
+            <div class="retro-citation-div">&mdash; Philip Roth</div>
+        </div>
+
+        <div class="retro-quote-div col-12 col-md-6">
+            <div class="retro-content-div">He almost waved away the tangible beauty of
+                the hour like a crowd of persistent
+                friends,
+                almost said to the lingering afterglow: 'Thank you, thank you, I'll be
+                all right.'
+            </div>
+            <div class="retro-citation-div">&mdash; Boris Pasternak, <span
+                        class="retro-title-span">Doctor Zhivago</span></div>
+        </div>
+
+        <div class="retro-quote-div col-12 col-md-6">
+            <div class="retro-content-div">'One whom the dragons will speak with,'
+                he said, 'that is a
+                dragonlord, or at least that is the center of the matter. It's not a
+                trick of mastering the dragons, as most people think. Dragons have
+                no
+                masters.'
+            </div>
+            <div class="retro-citation-div">&mdash; Ged, <span class="retro-title-span">The Tombs of Atuan</span>
+                , Ursula K Le Guin
+            </div>
+        </div>
+
+        <div class="retro-quote-div col-12 col-md-6">
+            <div class="retro-content-div">Was it me said you were crazy?</div>
+            <div class="retro-citation-div">&mdash; Kate Bush, <span
+                        class="retro-title-span">Rocket's Tail</span></div>
+        </div>
+    </div>
 </div>
-</font>
+
+<script type="text/javascript">
+
+    var _gaq = _gaq || [];
+    _gaq.push(['_setAccount', 'UA-8450480-1']);
+    _gaq.push(['_trackPageview']);
+
+    (function () {
+        var ga = document.createElement('script');
+        ga.type = 'text/javascript';
+        ga.async = true;
+        ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+        var s = document.getElementsByTagName('script')[0];
+        s.parentNode.insertBefore(ga, s);
+    })();
+
+</script>
+<?php wp_head(); ?>
 </body>
 </html>
 
