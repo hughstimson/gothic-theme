8ec8390683fc89bafe741795c23e805a45a48335
Hugh Stimson <hstimson@essa.com>
2025-11-16 15:24:34 -0800

    Remove Linkedin/ESSA/Geocology links and all quotes from landing page.

--- retro-landing.php
+++ retro-landing.php
@@ -16,105 +16,10 @@
             <img src="<?php bloginfo('template_url'); ?>/images/star.70.png"/>
             <p>
                 <a href="https://mstdn.ca/@hughstimson" rel="me">mastodon</a> |
-		<!-- <a href="//twitter.com/hughstimson">twitter</a> | -->
                 <a href="twitter/">twitter archive</a> | 
                 <a href="<?php bloginfo('url'); ?>/blog">weblog</a>
 
             </p>
-            <p>
-                <a href="//ca.linkedin.com/in/hughstimson">resume</a> |
-                <a href="//essa.com">job</a> |
-                <a href="//geocology.ca">old job</a>
-            </p>
-        </div>
-    </div>
-    <div class="row">
-        <div class="retro-quote-div col-12 col-lg-6">
-            <div class="retro-content-div">We have to learn what we can, but remain mindful that our knowledge not
-                close the
-                circle,
-                closing out the void, so that we forget that what we do not know remains boundless, without
-                limit or bottom, and that what we know may have to share the quality of being known with
-                what denies it. What is seen with one eye has no depth.
-            </div>
-            <div class="retro-citation-div">&mdash; Stone Telling, <span
-                        class="retro-title-span">Always Coming Home</span>, Ursula K. Leguin
-            </div>
-        </div>
-
-        <div class="retro-quote-div col-12 col-lg-6">
-            <div class="retro-content-div">Civilizations in decline are consistently characterized by a tendency
-                toward standardization and uniformity. Conversely, during the growth
-                stage of civilization, the tendency is toward differentiation and
-                diversity.
-            </div>
-            <div class="retro-citation-div">
-                &mdash; Arnold Toynbee, <span class="retro-title-span">A Study of History</span>
-            </div>
-        </div>
-
-        <div class="retro-quote-div col-12 col-lg-6">
-            <div class="retro-content-div">As the piston continues to rise, creating vacuum in the crankcase,
-                the
-                inlet port is uncovered and thus the cycle is completed and repeated.
-            </div>
-            <div class="retro-citation-div">&mdash; "The Two Stroke Engine", <span class="retro-title-span">Haynes Motorcyle Basics Techbook</span>
-            </div>
-        </div>
-
-        <div class="retro-quote-div col-12 col-lg-6">
-            <div class="retro-content-div">If we knew what it was we were doing, it would not be called research.</div>
-            <div class="retro-citation-div">&mdash; Albert Einstein</div>
-        </div>
-        <div class="retro-quote-div col-12 col-lg-6">
-            <div class="retro-content-div">I certainly had no feeling for harmony, and Schoenberg
-                thought that
-                that would make it impossible for me to write music. He said, 'You'll
-                come to a wall you won't be able to get through.' So I said, 'I'll
-                beat my head against that wall.'
-            </div>
-            <div class="retro-citation-div">&mdash; John Cage</div>
-        </div>
-        
-        <div class="retro-quote-div col-12 col-lg-6">
-            <div class="retro-content-div">It is an ultimate act of rudeness to find fault with
-                anything which is given to us in a
-                spirit of love. So a modern, secular education is often painful. By it's very
-                nature, it
-                invites us to question the wisdom of the ones we love. Too bad.
-            </div>
-            <div class="retro-citation-div">&mdash; Kurt Vonnegut, address to the
-                graduating class of Hobart and William Smith Colleges, Geneva, New York
-                1974, from <span class="retro-title-span">Palm
-                    Sunday</span></div>
-        </div>
-
-        <div class="retro-quote-div col-12 col-lg-6">
-            <div class="retro-content-div">Life is not ever either or. It's and and and and
-                and.
-            </div>
-            <div class="retro-citation-div">&mdash; Philip Roth</div>
-        </div>
-        <div class="retro-quote-div col-12 col-lg-6">
-            <div class="retro-content-div">'One whom the dragons will speak with,'
-                he said, 'that is a
-                dragonlord, or at least that is the center of the matter. It's not a
-                trick of mastering the dragons, as most people think.'
-            </div>
-            <div class="retro-citation-div">&mdash; Ged, <span class="retro-title-span">The Tombs of Atuan</span>
-                , Ursula K Le Guin
-            </div>
-        </div>
-        <div class="retro-quote-div col-12 col-lg-6">
-            <div class="retro-content-div">A superior pilot uses his superior judgment to avoid situations which require the use of his superior skill.
-            </div>
-            <div class="retro-citation-div">&mdash; Frank Borman, astronaut</div>
-        </div>
-        <div class="retro-quote-div col-12 col-lg-6">
-            <div class="retro-content-div">He knew the difference between knowing the name of something and knowing something.
-            </div>
-            <div class="retro-citation-div">&mdash; Richard Feynman, referring to his father.
-            </div>
         </div>
     </div>
 </div>
