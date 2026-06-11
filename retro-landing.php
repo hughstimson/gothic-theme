993b554ac36ae2cd145e5262245d803e8a6f1c75
Hugh Stimson <hstimson@essa.com>
2026-06-10 17:29:05 -0700

    Drop Bootstrap from the landing page and greatly simplify CSS.
    If quotes get added back to the landing page the CSS might have to be elaborated again somewhat, but there's no need for Bootstrap.

--- retro-landing.php
+++ retro-landing.php
@@ -3,25 +3,17 @@
 <head>
     <title>hughstimson.org</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet"
-          href="<?php bloginfo('template_url'); ?>/lib/bootstrap-4.0.0-beta.2-dist/css/bootstrap-grid.min.css"
-          type="text/css" media="screen">
     <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>" type="text/css" media="screen"/>
 </head>
 <body>
-<div class="container" id="retro-container">
-    <div class="row">
-        <div class="col" id="retro-masthead-div">
-            <h1 id="retro-title-h1">hughstimson.org</h1>
-            <img src="<?php bloginfo('template_url'); ?>/images/star.70.png"/>
-            <p>
-                <a href="https://mstdn.ca/@hughstimson" rel="me">mastodon</a> |
-                <a href="twitter/">twitter archive</a> | 
-                <a href="<?php bloginfo('url'); ?>/blog">weblog</a>
-
-            </p>
-        </div>
-    </div>
+<div id="retro-container">
+    <h1>hughstimson.org</h1>
+    <img src="<?php bloginfo('template_url'); ?>/images/star.70.png"/>
+    <p>
+        <a href="https://mstdn.ca/@hughstimson" rel="me">mastodon</a> |
+        <a href="twitter/">twitter archive</a> |
+        <a href="<?php bloginfo('url'); ?>/blog">weblog</a>
+    </p>
 </div>
 
 <script type="text/javascript">
